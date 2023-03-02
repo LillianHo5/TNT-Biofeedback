43bef2986451f4326850fd8e439503dac87e539c
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2023-03-01 21:06:48 -0800

    Resolved bug with SignUp display

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignUp from './src/screens/SignUp.js';
@@ -8,30 +8,29 @@ const Stack = createNativeStackNavigator();
 
 const MyStack = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Sign Up"
-          component={SignUp}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Sign Up"
+        component={SignUp}
+      />
+    </Stack.Navigator>
   );
 };
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <MyStack />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <MyStack />
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0)',
+    flex: 1
   },
 });
